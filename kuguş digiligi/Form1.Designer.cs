354bc3b73c0f422321816b31ae4c3a8051591caa
Batuhan Demir <batuhandemir2005@gmail.com>
2020-06-30 12:26:05 +0300

    Kuş Dili Ver1

---
+++ kuguş digiligi/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace kuguş_digiligi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.girilen = new System.Windows.Forms.RichTextBox();
+            this.çıktı = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKus = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // girilen
+            // 
+            this.girilen.Location = new System.Drawing.Point(92, 39);
+            this.girilen.Name = "girilen";
+            this.girilen.Size = new System.Drawing.Size(369, 108);
+            this.girilen.TabIndex = 0;
+            this.girilen.Text = "";
+            // 
+            // çıktı
+            // 
+            this.çıktı.Location = new System.Drawing.Point(92, 243);
+            this.çıktı.Name = "çıktı";
+            this.çıktı.ReadOnly = true;
+            this.çıktı.Size = new System.Drawing.Size(369, 58);
+            this.çıktı.TabIndex = 1;
+            this.çıktı.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(92, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Normal Yazıdan Kuş Diline";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(25, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Yazı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(24, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Çıktı:";
+            // 
+            // txtKus
+            // 
+            this.txtKus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKus.Location = new System.Drawing.Point(495, 189);
+            this.txtKus.MaxLength = 10;
+            this.txtKus.Name = "txtKus";
+            this.txtKus.Size = new System.Drawing.Size(105, 20);
+            this.txtKus.TabIndex = 4;
+            this.txtKus.Text = "g";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.BorderSize = 3;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(280, 171);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(181, 38);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Kuş Dilinden Normal Yazıya";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(504, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ayıraç:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderSize = 3;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(0, 346);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Font Seç";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(395, 304);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Kopyala";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::kuguş_digiligi.Properties.Resources.porcay;
+            this.pictureBox1.Location = new System.Drawing.Point(525, 304);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(114, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox1.Location = new System.Drawing.Point(118, 367);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(343, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Kopyalanan Metni Otomatik Olarak Yazı Kısmına Yapıştır";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(467, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "<= Temizle";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(622, 384);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtKus);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.çıktı);
+            this.Controls.Add(this.girilen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kuş Dili Çevirici -ßy u/batu-_-batu-";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox girilen;
+        private System.Windows.Forms.RichTextBox çıktı;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKus;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ kuguş digiligi/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kuguş_digiligi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        char[] unluler = new char[8] { 'a', 'e', 'u', 'ü', 'ı', 'i', 'o', 'ö' };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string metin = girilen.Text;
+            foreach (char i in girilen.Text)
+            {
+                metin += i;
+                foreach (char unlu in unluler)
+                {
+                    if (i == unlu)
+                    {
+                        metin += txtKus.Text + i;
+                    }
+
+                }
+            }
+            metin = metin.Substring(girilen.Text.Length, metin.Length - girilen.Text.Length);
+            çıktı.Text = metin;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string metin = girilen.Text;
+            if (string.IsNullOrWhiteSpace(metin)) MessageBox.Show("Boş Yazı!", "Geçersiz", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                string total = "";
+                string[] ayrilacak = metin.Split(new[] { txtKus.Text }, StringSplitOptions.None);
+                foreach (string sa in ayrilacak)
+                {
+                    if (!string.IsNullOrWhiteSpace(sa))
+                    {
+                        total += sa.Substring(0, sa.Length - 1);
+                    }
+                }
+                total += metin.Substring(metin.Length - 1, 1);
+
+                if (total == metin)
+                    MessageBox.Show("Bu Metin Kuş Dilinde Yazılmamış veya Yanlış Ayıraç Seçmişsiniz!", "u/batu-_-batu", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                çıktı.Text = total;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+         DialogResult sa = fontDialog1.ShowDialog();
+            if(sa == DialogResult.OK)
+            {
+                foreach(Control kontrol in Controls)
+                {
+                    if (kontrol is TextBox || kontrol is RichTextBox || kontrol is Label || kontrol is Button || kontrol is CheckBox)
+                        kontrol.Font = fontDialog1.Font;
+                }
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(çıktı.Text))
+                MessageBox.Show("Boş Bir Yazıyı Kopyalamak Biraz Saçma Gibi Sanki?", "u/batu-_-batu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            else
+            {
+                Clipboard.SetText(çıktı.Text);
+
+                Random rnd = new Random();
+                label4.ForeColor = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+                //MessageBox.Show("Başarıyla Kopyalandı!", "u/batu-_-batu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+        string sonKopyalanan;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                string kopya = Clipboard.GetText();
+
+                if (kopya == "<= Temizle" || kopya == "Kopyala")
+                    Clipboard.SetText(sonKopyalanan);
+
+                if(sonKopyalanan != kopya)
+                {
+                    girilen.Text = kopya;
+
+                    sonKopyalanan = kopya;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = checkBox1.Checked;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            girilen.Text = "";
+
+            Random rnd = new Random();
+            label5.ForeColor = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.reddit.com/r/burdurland/");
+        }
+    }
+}
